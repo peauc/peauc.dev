8d6f96877841d733348b9d0078819058262ce910
Clement Peau <clement.peau@epitech.eu>
2016-08-19 19:54:41 +0200

    Updating base

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,8 @@ import ReactDOM from "react-dom";
 
 import Home from "./Components/Home.js"
 
-var page = document.getElementById("app");
-
 ReactDOM.render(
     <div>
-        {toto}
         <Home />
     </div>
-, page);
+    , document.getElementById("app"));
