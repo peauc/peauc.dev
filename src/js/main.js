755e36922fedb6ccf1fdf9758d92770de0ed8a34
Clement Peau <clement.peau@epitech.eu>
2016-08-19 20:27:36 +0200

    Updating base Trying the build on linux

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from "react"
+import ReactDOM from "react-dom"
 
-import Home from "./Components/Home.js"
+var Greeting = React.createClass({
+    render: function() {
+        return (
+            <p>{this.props.message}</p>
+        );
+    }
+});
 
-ReactDOM.render(
-    <div>
-        <Home />
-    </div>
-    , document.getElementById("app"));
+setInterval(function() {
+    var messages = ['Hello, World', 'Hello, Planet', 'Hello, Universe'];
+    var randomMessage = messages[Math.floor((Math.random() * 3))];
+
+    ReactDOM.render(
+        <div>
+            <Greeting message={randomMessage}/>
+        </div>
+        , document.getElementById('greeting-div')
+    );
+}, 2000);
