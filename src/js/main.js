74f0f75217f634953ef5df6d95d99f995b38e694
Clement Peau <clement.peau@epitech.eu>
2016-08-19 19:15:58 +0200

    Core project

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Home from "./Components/Home.js"
+
+var page = document.getElementById("app");
+
+ReactDOM.render(
+    <div>
+        {toto}
+        <Home />
+    </div>
+, page);
